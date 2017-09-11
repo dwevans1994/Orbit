f3f6ba19ab0879d78cab428dd704fb4b4dec8a8c
Ben Woodhouse <bwoodhouse322@gmail.com>
2017-09-11 20:12:23 +0000

    Add files via upload
    "download ddp client@0.1.1 and then in the file node_modules/ddp_client/index.js change "localhost" to ip address of computer"

---
+++ App.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import DDPClient from 'ddp-client';
+import { StyleSheet, Text, View, Image, TextInput, Slider } from 'react-native';
+
+let ddpClient = new DDPClient();
+
+import CheckBox from 'react-native-checkbox-heaven';
+import Gender from './gender.js'
+
+
+
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { age: 18, checked: false };
+  }
+
+  render() {
+
+    return (
+      <View style={[styles.container, { backgroundColor: '#250e36' }]}>
+        <View style={{
+          position: 'absolute',
+          flex: 1,
+          top: 80,
+        }}>
+          <Image source={require('./OrbitFinished.png')} style={{ width: 150, height: 65 }} />
+        </View>
+
+        <View >
+          <Text style={{ color: 'white', }} > Set age</Text>
+        </View>
+
+        <View>
+
+          <Slider
+            style={{ width: 300 }}
+            step={1}
+            minimumValue={18}
+            maximumValue={65}
+            value={this.state.age}
+            onValueChange={val => this.setState({ age: val })}
+
+          />
+
+        </View>
+        <View>
+          <Text style={{ color: 'white' }}>{this.state.age}</Text>
+        </View>
+
+        <Gender />
+
+      </View>
+
+    );
+  }
+}
+
+ componentDidMount() {
+    ddpClient.connect((err, wasReconnect) => {
+      let connected = true;
+      if (err) connected = false;
+
+      this.setState({ connected: connected });
+    });
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
